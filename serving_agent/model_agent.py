58631fc0a83339ae829a2588f6c07c5e41190d2d
wwen <wenweihuang@yimian.com.cn>
2021-01-03 01:04:38 +0800

    feat: add model agent

--- serving_agent/model_agent.py
+++ serving_agent/model_agent.py
@@ -5,14 +5,28 @@ import redis
 
 
 class ModelAgent:
-    def __init__(
-        self, redis_broker='localhost:6367', ModelClass, ModelConfig={}, 
+    def __init__(self, redis_broker='localhost:6379', redis_queue='broker', model_class=None, model_config={}, batch_size=64, model_sleep=0.1):
+        parse = lambda x: {'host': x.split(':')[0], 'port': int(x.split(':')[1])}
+        self.db = redis.StrictRedis(**parse(redis_broker))
+        self.redis_queue = redis_queue
+        assert 'predict' in dir(model_class), 'No predict function in model class'
+        self.model_class = model_class
+        self.model_config = model_config
+        self.batch_size = batch_size
+        self.model_sleep = model_sleep
 
-    ):
-        self.listen
-        self.model = ModelClass(**ModelConfig)
-        pass
-
-    def run():
+    def run(self, pre_process=lambda x: x, post_process=lambda x: x):
+        model = self.model_class(**self.model_config)
+        print('model init')
         while True:
-            pass
+            with self.db.pipeline() as pipe:
+                pipe.lrange(self.redis_queue, 0, self.batch_size - 1)
+                pipe.ltrim(self.redis_queue, self.batch_size, -1)
+                queue, _ = pipe.execute()
+            if queue:
+                messages = [pickle.loads(x) for x in queue]
+                keys = [message.get('key') for message in messages]
+                model_inputs = [pre_process(message.get('model_input')) for message in messages]
+                results = [post_process(x) for x in model.predict(model_inputs)]
+                self.db.mset({key: pickle.dumps(result) for key, result in zip(keys, results)})
+            time.sleep(self.model_sleep)
