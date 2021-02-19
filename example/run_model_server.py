31105f12f6e14bd65ec1b10671d5683e2def34b5
wwen <wenweihuang@yimian.com.cn>
2021-02-19 10:43:09 +0800

    chore: change model collection default setting

--- example/run_model_server.py
+++ example/run_model_server.py
@@ -3,5 +3,5 @@ from serving_agent import ModelAgent
 from example.TestModel import TestModel
 
 if __name__ == "__main__":
-    model_agent = ModelAgent(redis_broker='localhost:6379', redis_queue='example', model_class=TestModel, collection=True, collection_limit=100)
+    model_agent = ModelAgent(redis_broker='localhost:6379', redis_queue='example', model_class=TestModel, collection=False, collection_limit=24000)
     model_agent.run()
